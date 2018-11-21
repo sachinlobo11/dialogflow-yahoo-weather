bd6d55787e85129575e08246e7148b1d7a0a6b9f
sachinlobo11 <30945603+sachinlobo11@users.noreply.github.com>
2018-11-21 11:00:37 +0530

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -40,7 +40,7 @@ app = Flask(__name__)
 
 @app.route('/w')
 def index():
-    return 'hello'
+    return '"hello"
 """@app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
