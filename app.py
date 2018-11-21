7d986cb59603c900d162b1dcb26d64580ec034a4
sachinlobo11 <30945603+sachinlobo11@users.noreply.github.com>
2018-11-21 14:03:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,10 +43,10 @@ def index():
     return "hello how are u test message"
 
 if __name__ == '__main__':
-    #port = int(os.getenv('PORT', 5000))
-   # print("Starting app on port %d" % port)
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
         app.run()
-   # app.run(debug=True, port=port, host='0.0.0.0')
+   app.run(debug=True, port=port, host='0.0.0.0')
 
 """@app.route('/webhook', methods=['POST'])
 def webhook():
