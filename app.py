be4adaf486db7127d0fe9a3bd6f6fa84faabd71d
sachinlobo11 <30945603+sachinlobo11@users.noreply.github.com>
2018-11-21 19:33:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,131 @@
+from __future__ import print_function
+#from future.standard_library import install_aliases
+#install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    # commented out by Naresh
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    print ("starting processRequest...")
+   # if req.get("result").get("action") != "yahooWeatherForecast":
+        #return {}
+    #baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    #yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = "https://api.thingspeak.com/channels/107478/feeds.json?results=1"
+    result = urlopen(yql_url).read()
+    #data = json.loads(result)
+    #for some the line above gives an error and hence decoding to utf-8 might help
+    data = json.loads(result.decode('utf-8'))
+    res = makeWebhookResult(data)
+    zap_url="https://hooks.zapier.com/hooks/catch/3174192/fdhs6r?dataq="+res
+    result1 = urlopen(zap_url).read()
+    return res
+
+
+"""def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"/"""
+
+
+def makeWebhookResult(data):
+    print ("starting makeWebhookResult...")
+    feeds = data.get('feeds')
+    if query is None:
+        return {}
+
+    field1 = feeds.get('field1')
+    if result is None:
+        return {}
+
+    
+
+    
+      
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today the water level of main tank is " + feeds.get('field1')
+    print("Response:")
+    print(speech)
+    return field1
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    return  "Hello there my friend !!"
+
+
+@app.route('/static_reply', methods=['POST'])
+def static_reply():
+    speech = "Hello there, this reply is from the webhook !! "
+    my_result =  {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+    res = json.dumps(my_result, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=True, port=port, host='0.0.0.0')
+
+
+
+
+
+
+
 # -*- coding:utf8 -*-
 # !/usr/bin/env python
 # Copyright 2017 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+"""# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
@@ -48,7 +170,7 @@ if __name__ == '__main__':
        
     app.run(debug=True, port=port, host='0.0.0.0')
 
-"""@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
 
